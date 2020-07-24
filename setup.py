2b1d74313c3a8e1a259af313e80c70353fc540ec
Roberto Metz <robertometz@robertometz.com>
2020-07-24 08:15:25 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+APP = ['gui.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'icon.icns',
+    'plist': {
+        'CFBundleShortVersionString': '0.2.0',
+        'LSUIElement': True,
+    },
+    'packages': ['PySimpleGUI', 'inforion', 'pandas'],
+}
+setup(
+    app=APP,
+    name='NewSmartdata',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], install_requires=['PySimpleGUI', 'inforion', 'pandas']
+)

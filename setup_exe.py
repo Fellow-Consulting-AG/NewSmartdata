1b971684f5b36cc4a520262ede5cabfd3f36d99b
Roberto Metz <robertometz@robertometz.com>
2020-07-25 15:54:56 -0400

    Add setup_exe.py

---
+++ setup_exe.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+	windows=[{
+		"script": "main.py",
+		# "icon_resources": [(1, "icon.ico")],
+	}],
+	options={
+		"py2exe": {
+			"unbuffered": True,
+			"optimize": 1,
+			"dist_dir": "installer/files",
+		}
+	},
+)

0e3ba390f437e126ff37abed6f029425886574ce
Restyled.io <commits@restyled.io>
2020-07-31 14:20:10 +0000

    Restyled by yapf

--- gui.py
+++ gui.py
@@ -60,76 +60,66 @@ def show_main():
             [
                 Frame(
                     "Logon information",
-                    [
-                        [
-                            Text(),
-                            Column(
-                                [
-                                    [
-                                        Text("ION File:", size=(14, 1)),
-                                        Input(key="-ION-FILE-"),
-                                        FileBrowse(
-                                            file_types=("ION API File", "*.ionapi")
-                                        ),
-                                    ],
-                                    [
-                                        Text("M3 Company/Div:"),
-                                        Input(key="-M3-COMPANY-", size=(5, 1)),
-                                        Input(key="-M3-DIV-", size=(7, 1)),
-                                    ],
-                                ]
-                            ),
-                        ]
-                    ],
+                    [[
+                        Text(),
+                        Column([
+                            [
+                                Text("ION File:", size=(14, 1)),
+                                Input(key="-ION-FILE-"),
+                                FileBrowse(file_types=("ION API File",
+                                                       "*.ionapi")),
+                            ],
+                            [
+                                Text("M3 Company/Div:"),
+                                Input(key="-M3-COMPANY-", size=(5, 1)),
+                                Input(key="-M3-DIV-", size=(7, 1)),
+                            ],
+                        ]),
+                    ]],
                 )
             ],
             # Information frame
             [
                 Frame(
                     "Input Data",
-                    [
-                        [
-                            Text(),
-                            Column(
-                                [
-                                    [
-                                        Text("URL:", size=(14, 1)),
-                                        Input(key="-ION-URL-"),
-                                    ],
-                                    [
-                                        Text("Program:", size=(14, 1)),
-                                        Input(key="-ION-Program-"),
-                                    ],
-                                    [
-                                        Text("Method:", size=(14, 1)),
-                                        Input(key="-ION-METHOD-"),
-                                    ],
-                                    [
-                                        Text("Input File:", size=(14, 1)),
-                                        Input(key="-INPUT-FILE-"),
-                                        FileBrowse(),
-                                    ],
-                                    [
-                                        Text("Output File:", size=(14, 1)),
-                                        Input(key="-OUTPUT-FILE-"),
-                                        FileBrowse(),
-                                    ],
-                                    [
-                                        Text("Begin on line:", size=(14, 1)),
-                                        Input(key="-ION-BEGIN-", enable_events=True),
-                                    ],
-                                    [
-                                        Text("End on line:", size=(14, 1)),
-                                        Input(key="-ION-END-", enable_events=True),
-                                    ],
-                                ],
-                            ),
-                        ]
-                    ],
+                    [[
+                        Text(),
+                        Column([
+                            [
+                                Text("URL:", size=(14, 1)),
+                                Input(key="-ION-URL-"),
+                            ],
+                            [
+                                Text("Program:", size=(14, 1)),
+                                Input(key="-ION-Program-"),
+                            ],
+                            [
+                                Text("Method:", size=(14, 1)),
+                                Input(key="-ION-METHOD-"),
+                            ],
+                            [
+                                Text("Input File:", size=(14, 1)),
+                                Input(key="-INPUT-FILE-"),
+                                FileBrowse(),
+                            ],
+                            [
+                                Text("Output File:", size=(14, 1)),
+                                Input(key="-OUTPUT-FILE-"),
+                                FileBrowse(),
+                            ],
+                            [
+                                Text("Begin on line:", size=(14, 1)),
+                                Input(key="-ION-BEGIN-", enable_events=True),
+                            ],
+                            [
+                                Text("End on line:", size=(14, 1)),
+                                Input(key="-ION-END-", enable_events=True),
+                            ],
+                        ], ),
+                    ]],
                 )
             ],
-        ],
-    )
+        ], )
 
     layout = [
         [sg.Menu(menu_def, tearoff=True, pad=(200, 1))],
@@ -270,42 +260,36 @@ def show_main():
             def TextLabel(text):
                 return sg.Text(text + ":", justification="r", size=(15, 1))
 
-            column = Column(
+            column = Column([
                 [
-                    [
-                        Frame(
-                            "Input Data",
-                            [
+                    Frame(
+                        "Input Data",
+                        [[
+                            Text(),
+                            Column([
                                 [
-                                    Text(),
-                                    Column(
-                                        [
-                                            [
-                                                TextLabel("Program"),
-                                                sg.Combo(
-                                                    programs,
-                                                    size=(10, 10),
-                                                    key="-PROGRAM-",
-                                                ),
-                                            ],
-                                            [
-                                                TextLabel("Output File"),
-                                                Input(key="-OUTPUT-FILE-"),
-                                                FileBrowse(),
-                                            ],
-                                        ],
+                                    TextLabel("Program"),
+                                    sg.Combo(
+                                        programs,
+                                        size=(10, 10),
+                                        key="-PROGRAM-",
                                     ),
-                                ]
-                            ],
-                        )
-                    ],
+                                ],
+                                [
+                                    TextLabel("Output File"),
+                                    Input(key="-OUTPUT-FILE-"),
+                                    FileBrowse(),
+                                ],
+                            ], ),
+                        ]],
+                    )
                 ],
-            )
+            ], )
 
             layout_extract = [[column], [Button("Execute"), Button("Cancel")]]
-            window_extract = sg.Window(
-                "New Smartdata  - Extract", layout_extract, margins=(10, 10)
-            )
+            window_extract = sg.Window("New Smartdata  - Extract",
+                                       layout_extract,
+                                       margins=(10, 10))
 
             while True:
                 event, values = window_extract.read()
@@ -318,7 +302,8 @@ def show_main():
                 if event == "Execute":
                     program = values["-PROGRAM-"]
                     output_file = values["-OUTPUT-FILE-"]
-                    excelexport.generate_api_template_file(program, output_file)
+                    excelexport.generate_api_template_file(
+                        program, output_file)
                     sg.popup("Template generated!")
 
         window.UnHide()
@@ -327,7 +312,8 @@ def show_main():
 
 
 def on_progress(total, processed):
-    sg.one_line_progress_meter("My 1-line progress meter", processed, total, "single")
+    sg.one_line_progress_meter("My 1-line progress meter", processed, total,
+                               "single")
 
 
 def open_about():
@@ -367,7 +353,9 @@ def save_settings(settings_file, settings, values):
             try:
                 settings[key] = values[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]]
             except Exception as e:
-                print(f"Problem updating settings from window values. Key = {key}")
+                print(
+                    f"Problem updating settings from window values. Key = {key}"
+                )
 
     with open(settings_file, "w") as f:
         jsondump(settings, f)

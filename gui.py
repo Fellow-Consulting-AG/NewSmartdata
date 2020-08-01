157da6442699ca13431e452ff28b23b64af4f601
Roberto Metz <robertometz@robertometz.com>
2020-08-01 16:53:43 -0400

    Update gui

--- gui.py
+++ gui.py
@@ -2,18 +2,17 @@ from json import dump as jsondump
 from json import load as jsonload
 from os import path
 
+import PySimpleGUI as sg
 import inforion as infor
 import pandas as pd
-import PySimpleGUI as sg
 import validators
-from inforion import excelexport
 from PySimpleGUI import Button
 from PySimpleGUI import Column
 from PySimpleGUI import FileBrowse
 from PySimpleGUI import Frame
 from PySimpleGUI import Input
 from PySimpleGUI import Text
-from validators import ValidationFailure
+from inforion import excelexport
 
 from _version import __version__
 

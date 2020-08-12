736352e3be8be97e00264f976b3796a3c4b44bbf
Roberto Metz <robertometz@robertometz.com>
2020-08-12 10:44:06 -0400

    Fix app build

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-
-pyinstaller --clean --noconsole --onefile --windowed -F gui.py --icon=iconfinder.icns --name QuickdataLoad
+rm -rf dist build
+pyinstaller --clean --noconsole --onefile --windowed -F gui.py --icon=iconfinder.icns --name Smartdata \
+    --add-binary "m3_fields_info.db:." \
+    --add-binary "Mapping_Template.xlsx:inforion/excelexport/" \
+    --add-binary "iconfinder.icns:."

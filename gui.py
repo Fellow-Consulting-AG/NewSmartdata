c1083bb6c6b0541f30b88a45d02b432fa10fea14
Restyled.io <commits@restyled.io>
2020-08-12 12:21:27 +0000

    Restyled by black

--- gui.py
+++ gui.py
@@ -27,8 +27,17 @@ sg.theme("LightGreen")
 sg.ChangeLookAndFeel("LightGreen")
 
 SETTINGS_FILE = path.join(path.dirname(__file__), r"settings.json")
-DEFAULT_SETTINGS = {"ion_file": None, "m3_company": 0, "m3_div": '', "url": '',
-                    "method": '', "input_file": '', "output_file": '', "start": 0, "end": None}
+DEFAULT_SETTINGS = {
+    "ion_file": None,
+    "m3_company": 0,
+    "m3_div": "",
+    "url": "",
+    "method": "",
+    "input_file": "",
+    "output_file": "",
+    "start": 0,
+    "end": None,
+}
 SETTINGS_KEYS_TO_ELEMENT_KEYS = {
     "ion_file": "-ION-FILE-",
     "m3_company": "-M3-COMPANY-",
@@ -71,80 +80,90 @@ def show_main():
             [
                 Frame(
                     "Logon information",
-                    [[
-                        Text(),
-                        Column([
-                            [
-                                Text("ION File:", size=(14, 1)),
-                                Input(key="-ION-FILE-"),
-                                FileBrowse(file_types=("ION API File",
-                                                       "*.ionapi")),
-                            ],
-                            [
-                                Text("M3 Company/Div:"),
-                                Input(key="-M3-COMPANY-", size=(5, 1)),
-                                Input(key="-M3-DIV-", size=(7, 1)),
-                            ],
-                        ]),
-                    ]],
+                    [
+                        [
+                            Text(),
+                            Column(
+                                [
+                                    [
+                                        Text("ION File:", size=(14, 1)),
+                                        Input(key="-ION-FILE-"),
+                                        FileBrowse(
+                                            file_types=("ION API File", "*.ionapi")
+                                        ),
+                                    ],
+                                    [
+                                        Text("M3 Company/Div:"),
+                                        Input(key="-M3-COMPANY-", size=(5, 1)),
+                                        Input(key="-M3-DIV-", size=(7, 1)),
+                                    ],
+                                ]
+                            ),
+                        ]
+                    ],
                 )
             ],
             # Information frame
             [
                 Frame(
                     "Input Data",
-                    [[
-                        Text(),
-                        Column([
-                            [
-                                Text("URL:", size=(14, 1)),
-                                Input(key="-ION-URL-"),
-                            ],
-                            [
-                                Text("Program:", size=(14, 1)),
-                                sg.Input(
-                                    size=(10, 1),
-                                    enable_events=True,
-                                    key="-ION-FILTER-",
-                                ),
-                            ],
-                            [
-                                Text(justification="r", size=(14, 1)),
-                                sg.Listbox(
-                                    programs,
-                                    size=(10, 5),
-                                    enable_events=False,
-                                    key="-ION-Program-",
-                                    select_mode="single",
-                                ),
-                            ],
-                            [
-                                Text("Method:", size=(14, 1)),
-                                Input(key="-ION-METHOD-"),
-                            ],
-                            [
-                                Text("Input File:", size=(14, 1)),
-                                Input(key="-INPUT-FILE-"),
-                                FileBrowse(),
-                            ],
-                            [
-                                Text("Output File:", size=(14, 1)),
-                                Input(key="-OUTPUT-FILE-"),
-                                FileBrowse(),
-                            ],
-                            [
-                                Text("Begin on line:", size=(14, 1)),
-                                Input(key="-ION-BEGIN-", enable_events=True),
-                            ],
-                            [
-                                Text("End on line:", size=(14, 1)),
-                                Input(key="-ION-END-", enable_events=True),
-                            ],
-                        ], ),
-                    ]],
+                    [
+                        [
+                            Text(),
+                            Column(
+                                [
+                                    [
+                                        Text("URL:", size=(14, 1)),
+                                        Input(key="-ION-URL-"),
+                                    ],
+                                    [
+                                        Text("Program:", size=(14, 1)),
+                                        sg.Input(
+                                            size=(10, 1),
+                                            enable_events=True,
+                                            key="-ION-FILTER-",
+                                        ),
+                                    ],
+                                    [
+                                        Text(justification="r", size=(14, 1)),
+                                        sg.Listbox(
+                                            programs,
+                                            size=(10, 5),
+                                            enable_events=False,
+                                            key="-ION-Program-",
+                                            select_mode="single",
+                                        ),
+                                    ],
+                                    [
+                                        Text("Method:", size=(14, 1)),
+                                        Input(key="-ION-METHOD-"),
+                                    ],
+                                    [
+                                        Text("Input File:", size=(14, 1)),
+                                        Input(key="-INPUT-FILE-"),
+                                        FileBrowse(),
+                                    ],
+                                    [
+                                        Text("Output File:", size=(14, 1)),
+                                        Input(key="-OUTPUT-FILE-"),
+                                        FileBrowse(),
+                                    ],
+                                    [
+                                        Text("Begin on line:", size=(14, 1)),
+                                        Input(key="-ION-BEGIN-", enable_events=True),
+                                    ],
+                                    [
+                                        Text("End on line:", size=(14, 1)),
+                                        Input(key="-ION-END-", enable_events=True),
+                                    ],
+                                ],
+                            ),
+                        ]
+                    ],
                 )
             ],
-        ], )
+        ],
+    )
 
     layout = [
         [sg.Menu(menu_def, tearoff=True, pad=(200, 1))],
@@ -298,46 +317,54 @@ def show_main():
             def TextLabel(text):
                 return sg.Text(text + ":", justification="r", size=(12, 1))
 
-            column = Column([
+            column = Column(
                 [
-                    Frame(
-                        "Input Data",
-                        [[
-                            Text(),
-                            Column([
+                    [
+                        Frame(
+                            "Input Data",
+                            [
                                 [
-                                    TextLabel("Program"),
-                                    sg.Input(
-                                        size=(10, 1),
-                                        enable_events=True,
-                                        key="-FILTER-",
+                                    Text(),
+                                    Column(
+                                        [
+                                            [
+                                                TextLabel("Program"),
+                                                sg.Input(
+                                                    size=(10, 1),
+                                                    enable_events=True,
+                                                    key="-FILTER-",
+                                                ),
+                                            ],
+                                            [
+                                                Text(justification="r", size=(12, 1)),
+                                                sg.Listbox(
+                                                    programs,
+                                                    size=(10, 5),
+                                                    enable_events=False,
+                                                    key="-PROGRAM-",
+                                                    select_mode="multiple",
+                                                ),
+                                            ],
+                                            [
+                                                TextLabel("Output Folder"),
+                                                sg.Input(key="-OUTPUT-FOLDER-"),
+                                                sg.FolderBrowse(
+                                                    target="-OUTPUT-FOLDER-"
+                                                ),
+                                            ],
+                                        ],
                                     ),
-                                ],
-                                [
-                                    Text(justification="r", size=(12, 1)),
-                                    sg.Listbox(
-                                        programs,
-                                        size=(10, 5),
-                                        enable_events=False,
-                                        key="-PROGRAM-",
-                                        select_mode="multiple",
-                                    ),
-                                ],
-                                [
-                                    TextLabel("Output Folder"),
-                                    sg.Input(key="-OUTPUT-FOLDER-"),
-                                    sg.FolderBrowse(target="-OUTPUT-FOLDER-"),
-                                ],
-                            ], ),
-                        ]],
-                    )
+                                ]
+                            ],
+                        )
+                    ],
                 ],
-            ], )
+            )
 
             layout_extract = [[column], [Button("Execute"), Button("Cancel")]]
-            window_extract = sg.Window("SmartData  - Extract",
-                                       layout_extract,
-                                       margins=(10, 10))
+            window_extract = sg.Window(
+                "SmartData  - Extract", layout_extract, margins=(10, 10)
+            )
 
             while True:
                 event, values = window_extract.read()
@@ -358,13 +385,12 @@ def show_main():
                     programs_list = values["-PROGRAM-"]
                     output_folder = values["-OUTPUT-FOLDER-"]
 
-                    if validators.length(programs_list,
-                                         1) and validators.length(
-                                             output_folder, 1):
+                    if validators.length(programs_list, 1) and validators.length(
+                        output_folder, 1
+                    ):
                         for program in programs_list:
                             output_path = output_folder + os.sep + program
-                            excelexport.generate_api_template_file(
-                                program, output_path)
+                            excelexport.generate_api_template_file(program, output_path)
                         sg.popup("Template(s) generated!")
                     else:
                         sg.popup_ok("Please, check the form values!")
@@ -375,8 +401,7 @@ def show_main():
 
 
 def on_progress(total, processed):
-    sg.one_line_progress_meter("My 1-line progress meter", processed, total,
-                               "single")
+    sg.one_line_progress_meter("My 1-line progress meter", processed, total, "single")
 
 
 def open_about():
@@ -397,13 +422,13 @@ def open_help():
             command = "/usr/bin/google-chrome-stable {}".format(docs)
         elif sys.platform.startswith("darwin"):
             command = "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome {}".format(
-                docs)
+                docs
+            )
         elif sys.platform.startswith("win"):
             command = "start chrome {}".format(docs)
-        subprocess.Popen(command,
-                         shell=True,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
+        subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
     except Exception as e:
         sg.popup("Cannot open browser.")
 
@@ -431,9 +456,7 @@ def save_settings(notify, settings_file, settings, values):
             try:
                 settings[key] = values[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]]
             except Exception as e:
-                print(
-                    f"Problem updating settings from window values. Key = {key}"
-                )
+                print(f"Problem updating settings from window values. Key = {key}")
 
     with open(settings_file, "w") as f:
         jsondump(settings, f)

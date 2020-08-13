17392e06573f41269ad4ec908f72568f1c9837f3
Roberto Metz <robertometz@robertometz.com>
2020-08-13 09:43:29 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 rm -rf dist build
+
 pyinstaller --clean --noconsole --onefile --windowed -F gui.py --icon=iconfinder.icns --name Smartdata \
     --add-data "m3_fields_info.db:." \
     --add-data "Mapping_Template.xlsx:inforion/excelexport/" \

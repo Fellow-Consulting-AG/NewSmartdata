00ff3be84e4f781839f8b3219a135d6451250d9b
Roberto Metz <robertometz@robertometz.com>
2020-08-13 09:33:38 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf dist build
 pyinstaller --clean --noconsole --onefile --windowed -F gui.py --icon=iconfinder.icns --name Smartdata \
-    --add-binary "m3_fields_info.db:." \
-    --add-binary "Mapping_Template.xlsx:inforion/excelexport/" \
-    --add-binary "iconfinder.icns:."
+    --add-data "m3_fields_info.db:." \
+    --add-data "Mapping_Template.xlsx:inforion/excelexport/" \
+    --add-data "iconfinder.icns:."

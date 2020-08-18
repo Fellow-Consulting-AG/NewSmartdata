82cde83989e4d6bfa950239bb6cebdf63d19e291
Roberto Metz <robertometz@robertometz.com>
2020-08-18 09:11:20 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf dist build
 
-pyinstaller --clean --noconsole --onefile --windowed -F gui.py --icon=iconfinder.icns --name QuickdataLoad \
+pyinstaller --clean --noconsole --onefile --windowed -F gui.pyw --icon=iconfinder.icns --name QuickdataLoad \
   --add-data "m3_fields_info.db:." \
   --add-data "Mapping_Template.xlsx:inforion/excelexport/" \
   --add-data "iconfinder.icns:."

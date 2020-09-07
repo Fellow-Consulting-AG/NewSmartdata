92b100bce58cb31e0f0d0e81e84eec7c65bd5cca
Restyled.io <commits@restyled.io>
2020-09-07 15:19:41 +0000

    Restyled by reorder-python-imports

--- gui.py
+++ gui.py
@@ -14,7 +14,12 @@ import PySimpleGUI as sg
 import validators
 from inforion import excelexport
 from inforion.transformation.transform import parallelize_tranformation
-from PySimpleGUI import Button, Column, FileBrowse, Frame, Input, Text
+from PySimpleGUI import Button
+from PySimpleGUI import Column
+from PySimpleGUI import FileBrowse
+from PySimpleGUI import Frame
+from PySimpleGUI import Input
+from PySimpleGUI import Text
 
 from _version import __version__
 from programs import programs
